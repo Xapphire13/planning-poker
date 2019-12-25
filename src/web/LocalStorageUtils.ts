0a184b37d72b32caab31ff8f107c13e68943cb8e
Steven Hobson-Campbell <xapphire13@live.com>
2019-12-25 00:43:41 -0800

    create user

---
+++ src/web/LocalStorageUtils.ts
@@ -0,0 +1,10 @@
+export default {
+  getItem<T>(key: string): T | undefined {
+    const itemJson = localStorage.getItem(key);
+    return itemJson && JSON.parse(itemJson);
+  },
+
+  setItem<T>(key: string, item: T): void {
+    localStorage.setItem(key, JSON.stringify(item));
+  }
+}
--- src/web/pages/WelcomePage.tsx
+++ src/web/pages/WelcomePage.tsx
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { RouteComponentProps } from "@reach/router";
 import UserDetailsInput from ":web/components/UserDetailsInput";
 import { PrimaryButton } from "office-ui-fabric-react/lib/Button";
 import { Stack, StackItem } from "office-ui-fabric-react/lib/Stack";
 import { createStylesFn } from ":shared/theme/createStylesFn";
 import useStyles from "react-with-styles/lib/hooks/useStyles";
+import User from ":shared/User";
+import uuid from "uuid/v4";
+import LocalStorageUtils from ":web/LocalStorageUtils";
+
 export type WelcomePageProps = RouteComponentProps;
 
 const stylesFn = createStylesFn(({ unit }) => ({
@@ -19,12 +23,37 @@ const stylesFn = createStylesFn(({ unit }) => ({
 export default function WelcomePage({ }: WelcomePageProps) {
   const { css, styles } = useStyles({ stylesFn });
   const [name, setName] = useState<string>();
+  const [userId, setUserId] = useState<string>();
+
+  useEffect(() => {
+    let user = LocalStorageUtils.getItem<User>("user");
+
+    if (user) {
+      setName(user.name);
+    } else {
+      user = {
+        id: uuid(),
+        name: ""
+      };
+
+      LocalStorageUtils.setItem("user", user);
+    }
+
+    setUserId(user.id);
+  }, []);
+
+  const handleJoin = () => {
+    LocalStorageUtils.setItem<User>("user", {
+      id: userId!,
+      name: name!
+    })
+  }
 
   return <Stack verticalFill>
     <StackItem grow {...css(styles.contentContainer)}>
       <h1 {...css(styles.heading)}>Planning Poker</h1>
       <UserDetailsInput name={name} onNameChanged={setName} />
     </StackItem>
-    <PrimaryButton disabled={!name}>Join</PrimaryButton>
+    <PrimaryButton disabled={!name || !userId} onClick={handleJoin}>Join</PrimaryButton>
   </Stack>;
 }
