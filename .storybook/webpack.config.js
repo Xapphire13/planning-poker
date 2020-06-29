dbe9505628526ca63c6c71a47d7ff4ed70b9b30c
Steven Hobson-Campbell <xapphire13@live.com>
2020-06-29 16:16:11 -0700

    Webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,31 +1,37 @@
 const path = require("path");
 const webpack = require("webpack");
+const merge = require("webpack-merge");
 
-module.exports = ({ config }) => {
-  config.module.rules = [{
-    test: /\.(ts|tsx)$/,
-    use: [
+module.exports = ({ config }) => merge(config, {
+  module: {
+    rules: [
       {
-        loader: "babel-loader",
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+          {
+            loader: 'linaria/loader',
+            options: {
+              sourceMap: process.env.NODE_ENV !== 'production'
+            }
+          }
+        ]
       },
-    ],
-  }];
-
-  config.resolve.alias = {
-    ":shared": path.resolve(__dirname, "../src/shared"),
-    ":server": path.resolve(__dirname, "../src/server"),
-    ":web": path.resolve(__dirname, "../src/web"),
-    ":storybook": path.resolve(__dirname, "../src/storybook"),
-    ":__generated__": path.resolve(__dirname, "../src/__generated__")
-  }
-
-  config.resolve.extensions.push('.ts', '.tsx');
-
-  config.plugins = [
-    ...(config.plugins || []),
+    ]
+  },
+  resolve: {
+    alias: {
+      ":shared": path.resolve(__dirname, "../src/shared"),
+      ":server": path.resolve(__dirname, "../src/server"),
+      ":web": path.resolve(__dirname, "../src/web"),
+      ":storybook": path.resolve(__dirname, "../src/storybook"),
+      ":__generated__": path.resolve(__dirname, "../src/__generated__")
+    },
+    extensions: [".ts", ".tsx"],
+  },
+  plugins: [
     new webpack.NamedModulesPlugin(),
     new (require("rewiremock/webpack/plugin"))()
-  ];
-
-  return config;
-};
+  ]
+});
